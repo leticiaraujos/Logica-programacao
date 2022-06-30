789b8e2507047ce8bd59e1cf5497976ff0cd44c7
Leticia Araujo <lettaraujos@hotmail.com>
2022-06-30 14:06:08 -0300

    inicio

---
+++ SistemaDeCadastro.js
@@ -0,0 +1,21 @@
+//Data do Evento
+let dataDoEvento = 29/06/2022
+if (dataDoEvento > 29/06/2022) {
+    console.log("Data válida, permitir evento!")
+} else {
+    console.log("Data inválida, cadastro não permitido")
+}
+//Idade dos participantes 
+let idadeParticipante = 17
+if (idadeParticipante >= 18) {
+    console.log("Idade válida")
+} else {
+    console.log("Idade menor do que permitido, cadastro inválido")
+}
+//Quantidade de participantes
+let quantParticipantes = 150
+if (quantParticipantes >= 100) {
+    console.log("Cadastro realizado com sucesso!")
+} else {
+    console.log("Alerta, quantidade de participantes excede o limite")
+}
